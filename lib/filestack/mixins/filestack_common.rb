37d13453d5b7197fee30973426197de59aa5290b
Huey Ly <huey@webmail.us>
2017-06-06 18:33:22 -0500

    updated tests with mocks

--- lib/filestack/mixins/filestack_common.rb
+++ lib/filestack/mixins/filestack_common.rb
@@ -1,42 +1,49 @@
-require "filestack/config"
-require "typhoeus"
+require 'filestack/config'
+require 'typhoeus'
 
+# Module is mixin for common functionalities that all Filestack
+# objects can call.
 module FilestackCommon
+  # Send the delete api request to delete a filelink
+  #
+  # @param [String]             file_handle   The Filelink handle
+  # @param [String]             apikey        Your Filestack API Key
+  # @param [FilestackSecurity]  security      Optional Filestack security object
+  #                                           if security is enabled.
+  #
+  # @return [Typhoeus::Response]
   def send_delete(file_handle, apikey, security = nil)
-    url = sprintf("%s/file/%s?key=%s",
-      FilestackConfig::API_URL,
-      file_handle,
-      apikey)
+    url = format('%s/file/%s?key=%s',
+                 FilestackConfig::API_URL,
+                 file_handle,
+                 apikey)
 
-    if !security.nil?
-      url = security.sign_url(url)
-    end
+    url = security.sign_url(url) unless security.nil?
 
-    response = Typhoeus.delete(url)
-    return response
+    Typhoeus.delete(url)
   end
 
+  # Send the overwrite api request to update a filelink
+  #
+  # @param [String]             filepath      filepath of file to upload
+  # @param [String]             file_handle   The Filelink handle
+  # @param [String]             apikey        Your Filestack API Key
+  # @param [FilestackSecurity]  security      Optional Filestack security object
+  #                                           if security is enabled.
+  #
+  # @return [Typhoeus::Response]
   def send_overwrite(filepath, file_handle, apikey, security = nil)
-    url = sprintf("%s/file/%s?key=%s",
-      FilestackConfig::API_URL,
-      file_handle,
-      apikey)
+    url = format('%s/file/%s?key=%s', FilestackConfig::API_URL,
+                 file_handle, apikey)
+    url = security.sign_url(url) unless security.nil?
 
-    if !security.nil?
-     url = security.sign_url(url)
-    end
-
-    file = File.open(filepath,"r")
+    file = File.open(filepath, 'r')
     contents = file.read
+    headers = { 'Content-Type' => 'application/octet-stream' }
 
-    request = Typhoeus::Request.new(
-      url,
-      headers: {'Content-Type' => 'application/octet-stream'},
-      method: :post,
-      body: contents
-    )
-    response = request.run
-
-    return response
+    request = Typhoeus::Request.new(url, headers: headers,
+                                         method: :post,
+                                         body: contents)
+    request.run
   end
 end
