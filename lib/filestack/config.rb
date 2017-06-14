346d13e770396a65ae92dc187582e669c9d41cbd
Richard Herbert <richard.alan.herbert@gmail.com>
2017-06-14 09:52:08 -0700

    add constants

--- lib/filestack/config.rb
+++ lib/filestack/config.rb
@@ -1,5 +1,25 @@
+require 'filestack/ruby/version'
+
+include Filestack
+
 class FilestackConfig
   API_URL = 'https://www.filestackapi.com/api'.freeze
   CDN_URL = 'https://cdn.filestackcontent.com'.freeze
   PROCESS_URL = 'https://process.filestackapi.com'.freeze
+
+  MULTIPART_START_URL = 'https://upload.filestackapi.com/multipart/start'.freeze
+  MULTIPART_UPLOAD_URL = 'https://upload.filestackapi.com/multipart/upload'.freeze
+  MULTIPART_COMPLETE_URL = 'https://upload.filestackapi.com/multipart/complete'.freeze
+
+  MULTIPART_PARAMS = %w[
+    store_location store_region store_container
+    store_path store_access
+  ].freeze
+
+  DEFAULT_CHUNK_SIZE = 5 * 1024**2
+  VERSION = Filestack::Ruby::VERSION
+  HEADERS = {
+    'User-Agent' => "filestack-python #{VERSION}",
+    'Filestack-Source' => "Python-#{VERSION}"
+  }.freeze
 end
