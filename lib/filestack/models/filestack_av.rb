b666befa050263e00df752f1d3ee474b28ab6cb5
Richard Herbert <richard.alan.herbert@gmail.com>
2017-06-20 08:02:11 -0700

    add AV class

---
+++ lib/filestack/models/filestack_av.rb
@@ -0,0 +1,32 @@
+require 'filestack/models/filelink'
+require 'filestack/utils/utils'
+
+# Class for AV objects -- allows to check status
+# and upgrade to filelink once completed
+class AV
+  include UploadUtils
+  attr_reader :apikey, :security
+
+  def initialize(url, apikey: nil, security: nil)
+    @url = url
+    @apikey = apikey
+    @secuirty = security
+  end
+
+  # Turns AV into filelink if video conversion is complete
+  #
+  # @return [Filestack::Filelink]
+  def to_filelink
+    return 'Video conversion incomplete' unless status == 'completed'
+    response = UploadUtils.make_call(@url, 'get').body
+    handle = response['data']['url'].split('/').last
+    Filelink.new(handle, apikey: @apikey, security: @security)
+  end
+
+  # Checks the status of the video conversion
+  #
+  # @return [String]
+  def status
+    UploadUtils.make_call(@url, 'get').body['status']
+  end
+end
